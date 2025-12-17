fd832244673bf85a9bd46ff6dee06caab7412d12
lxu <lawrencexu8@gmail.com>
2025-12-17 20:20:19 +0100

    standardizing endpoints

--- src/main/java/io/github/larrythexu/ElevatorEmu/ElevatorManager/ElevatorManagerController.java
+++ src/main/java/io/github/larrythexu/ElevatorEmu/ElevatorManager/ElevatorManagerController.java
@@ -4,13 +4,11 @@ import io.github.larrythexu.ElevatorEmu.Elevator.Elevator;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api")
 public class ElevatorManagerController {
 
   private final ElevatorManager elevatorManager;
